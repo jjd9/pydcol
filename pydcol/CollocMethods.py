bdd403e5263676d7e56141bbf5fd5e86beb128e2
jjd9 <32826253+jjd9@users.noreply.github.com>
2021-05-06 16:18:20 -0500

    Update CollocMethods.py

--- pydcol/CollocMethods.py
+++ pydcol/CollocMethods.py
@@ -5,12 +5,14 @@ Authors: John D'Angelo, Shreyas Sudhaman
 Date: 05/01/2021
 """
 
-TRAP = 0
-HERM = 1
-RADAU = 2
-EB = 3
-EF = 4
+TRAP = 0 # Trapezoid method, 1st order accurate
+HERM = 1 # Hermite-Simpson method, 4th order accurate
+RADAU = 2 # Radau IIA method, 3rd order accurate
+EB = 3 # Euler Backward method, 1st order accurate
+EF = 4 # Euler Forward method, 1st order accurate
 
+# midpoint methods are treated differently
 MIDPOINT_METHODS = [HERM, RADAU]
 
+# Useful to be able to map the methods to their names as strings
 METHOD_NAMES = ["Trapezoid", "Hermite-Simpson", "Radau", "Euler-Backward", "Euler-Forward"]
